6b25c006308e632f7cf3cd1a781accd91aee2854
Meghan Pund <meghan.pund@gmail.com>
2022-01-14 12:22:38 -0500

    initial commit

---
+++ linked_lists.py
@@ -0,0 +1,143 @@
+from multiprocessing.dummy import current_process
+from re import T
+
+
+class Node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes: data and the link the next node in the list.
+    """
+
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"<Node data: {self.data}>"
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        # ensures every list is empty upon creation
+        self.head = None
+
+    def is_empty(self):
+        # method to check that list is, indeed, empty (will return boolean value)
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes O(n) time
+        """
+        # runs in linear time (as opposed to constant time, like a normal list)
+        # visits each node and calls next until we reach the tail node
+        current = self.head
+        count  = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        """
+        Adds a new Node containing data at the head of the list
+        Takes O(1) time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Returns the node or 'None' if not found
+        
+        Takes O(n) time
+        """
+        
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a ned Node containing data at the next position
+        Insertion takes O(1) time, but finding the node at the insertion point takes O(n)
+
+        Takes overall O(n) time
+        """
+
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = Node.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or 'None' if the key doesn't exist
+        Takes O(n) time
+        """
+        
+        current = self.head
+        previous = None
+        found = False
+        
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        
+        return current
+
+    def __repr__(self):
+        """
+        Return a string representation of the list
+        Takes O(n) time
+        """
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append(f"[Head: {current.data}]")
+            elif current.next_node is None:
+                nodes.append(f"[Tail: {current.data}]")
+            else:
+                nodes.append(f"[{current.data}]")
+        
+            current = current.next_node
+        return '-> '.join(nodes)
