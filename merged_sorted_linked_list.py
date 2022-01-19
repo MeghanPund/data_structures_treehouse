5a253106caf12c06f48cc2ac069c0de36587248b
Meghan Pund <meghan.pund@gmail.com>
2022-01-19 17:25:50 -0500

    added time comPlexities are you happy Will?

--- merged_sorted_linked_list.py
+++ merged_sorted_linked_list.py
@@ -1,5 +1,6 @@
 from linked_lists import LinkedList
 
+
 def merge_sort(linked_list):
     """
     Sorts a linkd list in ascending order
@@ -7,28 +8,29 @@ def merge_sort(linked_list):
     - Repeatedly merge the sublists to produce sorted sublists until one remains
 
     Returns a sorted linked list
-    
+
     Takes O(kn log n)
     """
-    
+
     if linked_list.size() == 1:
         return linked_list
     elif linked_list.head is None:
         return linked_list
-      
+
     left_half, right_half = split(linked_list)
     left = merge_sort(left_half)
     right = merge_sort(right_half)
-    
+
     return merge(left, right)
-  
+
+
 def split(linked_list):
     """
     Divide the unsorted list at midpoint into sublists
     Takes O(k log n)
     """
-    
-    if linked_list == None or linked_list.head == None:
+
+    if linked_list is None or linked_list.head is None:
         left_half = linked_list
         right_half = None
         return left_half, right_half
@@ -36,35 +38,36 @@ def split(linked_list):
         size = linked_list.size()
         mid = size//2
         mid_node = linked_list.node_at_index(mid - 1)
-          
+
         left_half = linked_list
         right_half = LinkedList()
         right_half.head = mid_node.next_node
         mid_node.next_node = None
-          
+
         return left_half, right_half
-    
+
+
 def merge(left, right):
     """
     Merges two linked lists, sorting by data in nodes
-    
+
     Returns a new, merged list
     Takes O(n)
     """
-    
+
     # Create a new linked list that contains nodes from merging left and right
     merged = LinkedList()
-    
+
     # add a fake head that is discarded later
     merged.add(0)
-    
+
     # set current to the head of the linked list
     current = merged.head
-    
+
     # obtain head nodes for left and right linked lists
     left_head = left.head
     right_head = right.head
-    
+
     # iterate over left and right until we reach the tail node of item
     while left_head or right_head:
         # if head node of left is None, we're past the tail
@@ -96,12 +99,13 @@ def merge(left, right):
                 right_head = right_head.next_node
         # move current to next node
         current = current.next_node
-    
+
         # discard fake head and set first merged node as head
     head = merged.head.next_node
     merged.head = head
 
     return merged
+
 
 l = LinkedList()
 l.add(10)
